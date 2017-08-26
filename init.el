4b4dd011a806924fc06eb588e2f59cb116179186
Fabian Sorqvist <fabian@sorqvist.nu>
2017-08-26 12:00:21 +0200

    initial commit

---
+++ init.el
@@ -0,0 +1,83 @@
+(defvar base-dir (file-name-directory load-file-name)
+  "The root dir of the Emacs config.")
+
+(defvar base-savefile-dir (file-name-directory load-file-name)
+  "The root dir of the Emacs config.")
+
+(defvar modules-dir (expand-file-name "modules" base-dir)
+  "Additional modules")
+
+;; Add modules dir to path
+(add-to-list 'load-path modules-dir)
+
+;; Always load newest byte code
+(setq load-prefer-newer t)
+
+;; reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+
+;; warn when opening files bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
+(setq-default indent-tabs-mode nil)   ;; don't use tabs to indent
+(setq-default tab-width 8)            ;; but maintain correct appearance
+
+;; Newline at end of file
+(setq require-final-newline t)
+
+;; delete the selection with a keypress
+(delete-selection-mode t)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; autosave the undo-tree history
+(setq undo-tree-history-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq undo-tree-auto-save-history t)
+
+;; revert buffers automatically when underlying files are changed externally
+(global-auto-revert-mode t)
+
+;; smart tab behavior - indent or complete
+(setq tab-always-indent 'complete)
+
+;; disable annoying blink-matching-paren
+(setq blink-matching-paren nil)
+
+;; savehist keeps track of some history
+(require 'savehist)
+(setq savehist-additional-variables
+      ;; search entries
+      '(search-ring regexp-search-ring)
+      ;; save every minute
+      savehist-autosave-interval 60
+      ;; keep the home clean
+      savehist-file (expand-file-name "savehist" base-savefile-dir))
+(savehist-mode +1)
+
+;; save recent files
+(require 'recentf)
+(setq recentf-save-file (expand-file-name "recentf" base-savefile-dir)
+      recentf-max-saved-items 500
+      recentf-max-menu-items 15
+      ;; disable recentf-cleanup on Emacs start, because it can cause
+      ;; problems with remote files
+      recentf-auto-cleanup 'never)
+
+;; use shift + arrow keys to switch between visible buffers
+(require 'windmove)
+(windmove-default-keybindings)
+
+;; No toolbar
+(tool-bar-mode -1)
+
+;; No scroll bars
+(custom-set-variables
+ '(scroll-bar-mode nil))
+
+(require 'mini-ido)
---
+++ modules/mini-ido.el
@@ -0,0 +1,16 @@
+(require 'ido)
+
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-max-prospects 10
+      ido-save-directory-list-file (expand-file-name "ido.hist" base-savefile-dir)
+      ido-default-file-method 'selected-window
+      ido-auto-merge-work-directories-length -1)
+(ido-mode +1)
+
+;; disable ido faces to see flx highlights
+(setq ido-use-faces nil)
+
+(provide 'mini-ido)
