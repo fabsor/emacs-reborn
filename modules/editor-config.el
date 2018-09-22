d7d719ca23d0d39e69ba543e56f9a527217c93d9
Fabian Sorqvist <fabian@sorqvist.nu>
2018-09-22 11:38:03 +0200

    Improved editor

--- modules/editor-config.el
+++ modules/editor-config.el
@@ -3,4 +3,12 @@
   :config
   (editorconfig-mode 1))
 
+(use-package company
+  :ensure t
+  :init (global-company-mode))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
 (provide 'editor-config)
