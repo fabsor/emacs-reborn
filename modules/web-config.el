c4026a1bb7e6ae832fdaf3bd1d1fc2d7e82d9302
Fabian Sorqvist <fabian@sorqvist.nu>
2018-09-22 11:38:43 +0200

    Change web config

--- modules/web-config.el
+++ modules/web-config.el
@@ -1,5 +1,17 @@
 (require 'use-package)
 
+(use-package php-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+  (add-to-list 'auto-mode-alist '("\\.module$" . php-mode))
+  (add-to-list 'auto-mode-alist '("\\.test$" . php-mode))
+  (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
+  (add-to-list 'auto-mode-alist '("\\.install$" . php-mode))
+  (add-to-list 'auto-mode-alist '("\\.profile$" . php-mode))
+  (add-to-list 'auto-mode-alist '("\\.engine$" . php-mode))
+  )
+  
 (use-package web-mode
   :ensure t
   :config
@@ -7,14 +19,8 @@
   (add-to-list 'auto-mode-alist '("\\.tsx$" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.ts$" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.js$" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.module$" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.test$" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.inc$" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.install$" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.profile$" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.engine$" . web-mode))
-  
+  (add-to-list 'auto-mode-alist '("\\.tpl.php$" . web-mode))
+
   (defun my-web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-code-indent-offset 2)
@@ -22,7 +28,7 @@
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-enable-auto-quoting nil)
   )
-(add-hook 'web-mode-hook  'my-web-mode-hook)
+  (add-hook 'web-mode-hook  'my-web-mode-hook)
 
   )
 
