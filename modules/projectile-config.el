e4395bf71e56d32f2eac73cfc9b43de6b2ec873a
Fabian Sorqvist <fabian@sorqvist.nu>
2018-10-07 10:33:29 +0200

    add ripgrep

--- modules/projectile-config.el
+++ modules/projectile-config.el
@@ -2,7 +2,13 @@
 
 (use-package projectile
   :ensure t
-  :bind ("C-x p" . projectile-find-file)
+  :bind
+  ("C-x p" . projectile-find-file)
+  ("C-x a" . projectile-ripgrep)
+  )
+
+(use-package ripgrep
+  :ensure t
   )
 
 (provide 'projectile-config)
