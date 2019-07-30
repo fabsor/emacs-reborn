2a47f87741fcc3c1be5e40c11e01d58bec5074b8
Fabian Sorqvist <fabian@sorqvist.nu>
2019-07-30 09:30:54 +0200

    add rjsx-mode

--- modules/lang-config.el
+++ modules/lang-config.el
@@ -21,4 +21,11 @@
   (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
   :ensure t)
 
+(use-package rjsx-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.js$" . rjsx-mode))
+  (add-hook 'rjsx-mode-hook #'setup-tide-mode) 
+  (flycheck-add-mode 'typescript-tslint 'rjsx-mode) 
+  :ensure t)
+
 (provide 'lang-config)
