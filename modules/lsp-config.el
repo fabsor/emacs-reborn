6bd3a34d84676c8147364ba240268fa04d0dcb9b
Fabian Sorqvist <fabian@sorqvist.nu>
2024-02-20 06:54:38 +0100

    fix

--- modules/lsp-config.el
+++ modules/lsp-config.el
@@ -24,16 +24,11 @@
   :ensure t
   :init
   (require 'web-mode)
-  (require 'flycheck-phpstan)
   (add-hook 'php-mode-hook #'lsp)
   :config
   (setq lsp-headerline-breadcrumb-enable nil)
   (add-hook 'web-mode #'lsp)
   (add-to-list 'lsp-language-id-configuration '(".*\\.tsx$" . "html"))
-  (defun php-lsp-flycheck ()
-    (flycheck-mode t)
-    (flycheck-add-next-checker 'lsp 'phpstan))
-  (add-hook 'lsp-after-open-hook  'php-lsp-flycheck)
   (setq lsp-file-watch-threshold 10000000)
   (setq lsp-intelephense-licence-key "00IXYCU0ORI7RQ7")
   (setq lsp-intelephense-files-associations ["*.php" "*.phtml" "*.module" "*.inc"]))
